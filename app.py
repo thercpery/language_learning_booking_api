d1c7eeb535c82dcb711bafc771117d2e66e48c46
Roy Christian Pery <thercpery@gmail.com>
2022-04-06 12:09:46 +0800

    added UserLogin and password hashing

--- app.py
+++ app.py
@@ -3,8 +3,10 @@ from flask import Flask
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Api
+from flask_jwt_extended import JWTManager
 # Internal imports
-from controllers.users import UserRegister
+from models.User import UserModel
+from controllers.users import UserRegister, UserLogin
 from db import db
 
 app = Flask(__name__)
@@ -12,14 +14,16 @@ app = Flask(__name__)
 app.config["SQL_ALCHEMY_MODIFICATIONS"] = True
 # app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:1234@localhost/language_learning_booking_app" # postgres database
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+app.config["JWT_SECRET_KEY"] = "sikretong-malupeeeet"
 
 CORS(app)
 db.init_app(app)
 api = Api(app)
+jwt = JWTManager(app)
 
 @app.before_first_request
 def init_db():
-    db.create_all()
+    UserModel.__table__.create(db.session.bind, checkfirst=True)
 
 @app.route("/")
 def index():
@@ -27,6 +31,7 @@ def index():
 
 # routes
 api.add_resource(UserRegister, "/api/users/register")
+api.add_resource(UserLogin, "/api/users/login")
 
 if __name__ == "__main__":
     app.run()
--- controllers/users.py
+++ controllers/users.py
@@ -1,5 +1,24 @@
 from flask_restful import Resource, reqparse
+from flask_jwt_extended import (
+    create_access_token,
+    create_refresh_token
+)
+from werkzeug.security import generate_password_hash, check_password_hash, safe_str_cmp
 from models.User import UserModel
+
+_user_parser = reqparse.RequestParser()
+_user_parser.add_argument("username",
+    type=str,
+    required=True,
+    help="Username is required"
+)
+
+_user_parser.add_argument("password",
+    type=str,
+    required=True,
+    help="Username is required"
+)
+
 
 class UserRegister(Resource):
     parser = reqparse.RequestParser()
@@ -47,10 +66,15 @@ class UserRegister(Resource):
         data = UserRegister.parser.parse_args()
 
         if UserModel.find_by_username(data["username"]) and UserModel.find_by_email(data["email"]):
+            # If there is an existing username or email.
             return False, 400
 
         if not data["is_admin"]:
+            # If is_admin is not indicated
             data["is_admin"] = False
+
+        # Encrypt password
+        data["password"] = generate_password_hash(data["password"], salt_length=10)
 
         user = UserModel(**data)
         user.save_to_db()
@@ -58,3 +82,30 @@ class UserRegister(Resource):
         return True, 201
 
     
+class UserLogin(Resource):
+    @classmethod
+    def post(cls):
+        """ 
+            This authenticates the user manually
+            1. Get the data from parser(request body).
+            2. Find the user in the database.
+            3. Check the password.
+            4. Create an access token.
+            5. Craete refresh token (will look at this later).
+            6. Return the access token
+        """
+        data = _user_parser.parse_args()
+
+        user = UserModel.find_by_username(data["username"])
+        password = generate_password_hash(data["password"], salt_length=10)
+        print(check_password_hash(user.password, data["password"]))
+        if user and check_password_hash(user.password, data["password"]):
+            access_token = create_access_token(identity=user.id, fresh=True)
+            refresh_token = create_refresh_token(identity=user.id)
+
+            return {
+                "access_token": access_token,
+                "refresh_token": refresh_token
+            }, 200
+
+        return False, 401
